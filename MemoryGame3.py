74a4d0c25a1211017089357579cf4242c1b9fb1c
SihamELBOUBAKRI <sihamelboubakri8@gmail.com>
2024-01-17 20:56:49 +0100

    LES PREMIERS ETAPS

---
+++ MemoryGame3.py
@@ -0,0 +1,12 @@
+from tkinter import *
+from tkinter import messagebox
+import random
+import time
+L = ["Apple","Lemon","Cherry","Banana","Kiwi","Melon","Mango","Grape"]
+L_length = len(L)
+game_end = 0
+dict_cards = {}
+clicked_cards = 0
+fst= ""
+scnd= ""
+start = time.time()
